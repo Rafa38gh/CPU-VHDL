dc75976d0ef640b8c007feef1cda4da7f51ff66b
RAFAEL SISCARO DE CARVALHO <rafael.sc3@puccampinas.edu.br>
2025-09-02 16:51:52 -0300

    teste

---
+++ CPU.vhd
@@ -0,0 +1,183 @@
+-- CPU Completa --
+
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+USE work.ULA_package.all;
+USE work.reg4_package.all;
+USE work.DECODER_package.all;
+USE work.BUFF_package.all;
+
+ENTITY CPU IS
+	PORT (CLK		:		IN STD_LOGIC;			
+			ENABLE	:		IN STD_LOGIC;
+			OPREG		:		IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+			OPCODE	:		IN STD_LOGIC_VECTOR(3 DOWNTO 0);		-- 4 bits mais significativos indicam os registradores
+			DATA		:		IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+			CLEAR		:		IN STD_LOGIC;
+			R1, R2, R3, R4	:	OUT STD_LOGIC_VECTOR(3 DOWNTO 0);
+			OVERFLOW	:		OUT STD_LOGIC);
+END CPU;
+			
+ARCHITECTURE LOGIC OF CPU IS
+	-- Barramento de dados --
+	SIGNAL DBUS		:		STD_LOGIC_VECTOR(3 DOWNTO 0);
+	
+	-- Sinais do DECODER --
+	SIGNAL REG1, REG2		:		STD_LOGIC_VECTOR(1 DOWNTO 0);
+	SIGNAL DECODE			:		STD_LOGIC_VECTOR(2 DOWNTO 0);
+	SIGNAL DDATA			:		STD_LOGIC_VECTOR(3 DOWNTO 0);
+	SIGNAL S1, S2			:		STD_LOGIC_VECTOR(3 DOWNTO 0);
+	
+	-- Sinais dos registradores de propósito específico --
+	SIGNAL RA_OUT, RB_OUT, RG_OUT	:		STD_LOGIC_VECTOR(3 DOWNTO 0);
+	
+	-- Sinais dos buffers --
+	SIGNAL BA_OUT, BB_OUT, BG_OUT	:		STD_LOGIC_VECTOR(3 DOWNTO 0);
+	SIGNAL ENA, ENB, ENG				:		STD_LOGIC;
+	
+	-- Sinais da ULA --
+	SIGNAL ULACODE			:		STD_LOGIC_VECTOR(3 DOWNTO 0);
+	SIGNAL ULA_OUT			:		STD_LOGIC_VECTOR(3 DOWNTO 0);
+	
+--=====================================================================================================
+	-- Inicializando máquina de estados --
+	TYPE STATE_TYPE IS (IDLE, LOAD, LOAD1, LOAD2, SWAP1, SWAP2, SWAP3, SWAP4, ULA1, ULA2, ULA3, ULA4, ULA5, ULA6, ULA7, ULA8);
+	SIGNAL W		:		STATE_TYPE;
+	
+	BEGIN
+--=====================================================================================================
+		-- INSTÂNCIAS
+		
+		-- DECODER --
+		DEC: DECODER PORT MAP(CLK, REG1, REG2, CLEAR, DECODE, DDATA, S1, S2, R1, R2, R3, R4);
+		
+		-- ULA --
+		ULAFINAL: ULA PORT MAP(ULACODE, BA_OUT, BB_OUT, ULA_OUT, OVERFLOW); 
+		
+		-- Registradores de propósito específico --
+		A: reg4 PORT MAP(CLK, S1, CLEAR, RA_OUT);
+		B: reg4 PORT MAP(CLK, S2, CLEAR, RB_OUT);
+		G: reg4 PORT MAP(CLK, ULA_OUT, CLEAR, RG_OUT);
+		
+		-- Buffers --
+		BA: BUFF PORT MAP(RA_OUT, ENA, BA_OUT);
+		BB: BUFF PORT MAP(RB_OUT, ENB, BB_OUT);
+		BG: BUFF PORT MAP(RG_OUT, ENG, BG_OUT);
+--======================================================================================================
+		-- Máquina de estados --
+		
+		PROCESS(CLK, ENABLE, OPCODE, CLEAR, OPREG)
+		BEGIN
+			IF CLEAR = '1' THEN
+				W <= IDLE;
+				
+			ELSIF CLK'EVENT AND CLK = '1' THEN
+			
+				CASE W IS
+				
+					WHEN IDLE =>
+						ENA <= '0';
+						ENB <= '0';
+						ENG <= '0';
+						
+						IF ENABLE = '1' THEN
+							
+							IF OPCODE = "1000" THEN
+								W <= LOAD;
+							
+							ELSIF OPCODE = "1001" THEN
+								W <= SWAP1;
+								
+							ELSIF OPCODE = "0001" OR OPCODE = "0010" OR OPCODE = "0011" OR OPCODE = "0100" OR OPCODE = "0101" OR OPCODE = "0110" OR OPCODE = "0111" THEN
+								W <= ULA1;
+								
+							END IF;
+							
+						ELSE
+							W <= IDLE;
+							
+						END IF;
+--=======================================================================================================================================================================
+					-- LOAD
+					WHEN LOAD =>
+						DBUS <= DATA;
+						REG2(0) <= OPREG(0);
+						REG2(1) <= OPREG(1);
+						W <= LOAD1;
+					
+					WHEN LOAD1 =>
+						DDATA <= DBUS;
+						W <= LOAD2;
+					
+					WHEN LOAD2 =>
+						DECODE <= "001";
+						W <= IDLE;
+						
+--=======================================================================================================================================================================
+					-- SWAP
+					WHEN SWAP1 =>
+						REG2(0) <= OPREG(0);
+						REG2(1) <= OPREG(1);
+						REG1(0) <= OPREG(2);
+						REG1(1) <= OPREG(3);
+						W <= SWAP2;
+						
+					WHEN SWAP2 =>
+						DECODE <= "010";
+						W <= SWAP3;
+						
+					WHEN SWAP3 =>
+						DECODE <= "011";
+						W <= SWAP4;
+					
+					WHEN SWAP4 =>
+						DECODE <= "100";
+						W <= IDLE;
+					
+--=======================================================================================================================================================================
+					-- AND
+					WHEN ULA1 =>
+						REG2(0) <= OPREG(0);
+						REG2(1) <= OPREG(1);
+						REG1(0) <= OPREG(2);
+						REG1(1) <= OPREG(3);
+						W <= ULA2;
+					
+					WHEN ULA2 =>
+						DECODE <= "101";
+						W <= ULA3;
+					
+					WHEN ULA3 =>
+						ENA <= '1';
+						ENB <= '1';
+						W <= ULA4;
+						
+					WHEN ULA4 =>
+						ULACODE <= OPCODE;
+						DECODE <= "000";
+						W <= ULA5;
+						
+					WHEN ULA5 =>
+						ENG <= '1';
+						W <= ULA6;
+					
+					WHEN ULA6 =>
+						DBUS <= BG_OUT;
+						W <= ULA7;
+						
+					WHEN ULA7 =>
+						DDATA <= DBUS;	
+						W <= ULA8;
+						
+					WHEN ULA8 =>
+						DECODE <= "111";
+						W <= IDLE;
+					
+				END CASE;
+				
+			END IF;
+			
+		END PROCESS;
+	
+		
+END LOGIC;
---
+++ CPU_FINAL.vhd
@@ -0,0 +1,139 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+USE work.CPU_package.all;
+
+ENTITY CPU_FINAL IS
+	PORT (SW		:		IN STD_LOGIC_VECTOR(17 DOWNTO 0);
+			Clock_50	:	IN STD_LOGIC;
+			HEX3		:	OUT STD_LOGIC_VECTOR(0 TO 6);
+			HEX2		:	OUT STD_LOGIC_VECTOR(0 TO 6);
+			HEX1		:	OUT STD_LOGIC_VECTOR(0 TO 6);
+			HEX0		:	OUT STD_LOGIC_VECTOR(0 TO 6);
+			LEDG8		:	OUT STD_LOGIC);
+END CPU_FINAL;
+
+ARCHITECTURE LOGIC OF CPU_FINAL IS
+	SIGNAL ENABLE		:		STD_LOGIC;
+	SIGNAL OPREG		:		STD_LOGIC_VECTOR(3 DOWNTO 0);
+	SIGNAL OPCODE		:		STD_LOGIC_VECTOR(3 DOWNTO 0);
+	SIGNAL DATA			:		STD_LOGIC_VECTOR(3 DOWNTO 0);
+	SIGNAL CLEAR		:		STD_LOGIC;
+	SIGNAL R1			:		STD_LOGIC_VECTOR(3 DOWNTO 0);
+	SIGNAL R2			:		STD_LOGIC_VECTOR(3 DOWNTO 0);
+	SIGNAL R3			:		STD_LOGIC_VECTOR(3 DOWNTO 0);
+	SIGNAL R4			:		STD_LOGIC_VECTOR(3 DOWNTO 0);
+	SIGNAL OVERFLOW	:		STD_LOGIC;
+	
+	BEGIN
+		
+		-- ENABLE --
+		ENABLE <= SW(17);
+		
+		-- OPREG --
+		OPREG(0) <= SW(9);
+		OPREG(1) <= SW(10);
+		OPREG(2) <= SW(11);
+		OPREG(3) <= SW(12);
+		
+		-- OPCODE --
+		OPCODE(0) <= SW(5);
+		OPCODE(1) <= SW(6);
+		OPCODE(2) <= SW(7);
+		OPCODE(3) <= SW(8);
+		
+		-- DATA --
+		DATA(0) <= SW(0);
+		DATA(1) <= SW(1);
+		DATA(2) <= SW(2);
+		DATA(3) <= SW(3);
+		
+		-- CLEAR --
+		CLEAR <= SW(16);
+		
+		-- OVERFLOW --
+		OVERFLOW <= LEDG8;
+		
+--=====================================================================
+
+		-- CPU --
+		CPUFINAL: CPU PORT MAP(Clock_50, ENABLE, OPREG, OPCODE, DATA, CLEAR, R1, R2, R3, R4, OVERFLOW);
+		
+--=====================================================================
+
+		-- DISPLAY --
+		WITH R1 SELECT
+			HEX3 <= 	"0000001" when "0000",
+						"1001111" when "0001",
+						"0010010" when "0010",
+						"0000110" when "0011",
+						"1001100" when "0100",
+						"0100100" when "0101",
+						"0100000" when "0110",
+						"0001111" when "0111",
+						"0000000" when "1000",
+						"0000100" when "1001",
+						"0001000" when "1010",
+						"1100000" when "1011",
+						"0110001" when "1100",
+						"1000010" when "1101",
+						"0110000" when "1110",
+						"0111000" when "1111",
+						"1111111" when others;
+			
+		WITH R2 SELECT
+			HEX2 <= 	"0000001" when "0000",
+						"1001111" when "0001",
+						"0010010" when "0010",
+						"0000110" when "0011",
+						"1001100" when "0100",
+						"0100100" when "0101",
+						"0100000" when "0110",
+						"0001111" when "0111",
+						"0000000" when "1000",
+						"0000100" when "1001",
+						"0001000" when "1010",
+						"1100000" when "1011",
+						"0110001" when "1100",
+						"1000010" when "1101",
+						"0110000" when "1110",
+						"0111000" when "1111",
+						"1111111" when others;
+		
+		WITH R3 SELECT
+			HEX1 <= 	"0000001" when "0000",
+						"1001111" when "0001",
+						"0010010" when "0010",
+						"0000110" when "0011",
+						"1001100" when "0100",
+						"0100100" when "0101",
+						"0100000" when "0110",
+						"0001111" when "0111",
+						"0000000" when "1000",
+						"0000100" when "1001",
+						"0001000" when "1010",
+						"1100000" when "1011",
+						"0110001" when "1100",
+						"1000010" when "1101",
+						"0110000" when "1110",
+						"0111000" when "1111",
+						"1111111" when others;
+						
+		WITH R4 SELECT
+			HEX0 <= 	"0000001" when "0000",
+						"1001111" when "0001",
+						"0010010" when "0010",
+						"0000110" when "0011",
+						"1001100" when "0100",
+						"0100100" when "0101",
+						"0100000" when "0110",
+						"0001111" when "0111",
+						"0000000" when "1000",
+						"0000100" when "1001",
+						"0001000" when "1010",
+						"1100000" when "1011",
+						"0110001" when "1100",
+						"1000010" when "1101",
+						"0110000" when "1110",
+						"0111000" when "1111",
+						"1111111" when others;
+END LOGIC;
